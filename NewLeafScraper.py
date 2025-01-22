81efeb887ab8b3a260271d6467641d864d761fcb
filleepa <78672802+filleepa@users.noreply.github.com>
2025-01-22 18:21:12 +0000

    Added in while loop that will let Selenium scroll to the bottom of the dynamically loaded webpage

--- NewLeafScraper.py
+++ NewLeafScraper.py
@@ -1,17 +1,34 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager  # Automatically manages ChromeDriver
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd 
+import time
+from bs4 import BeautifulSoup
 
-# ChromeDriver set up with Service
+
+#ChromeDriver set up with Service
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
-
 url = 'https://www.metacritic.com/game/animal-crossing-new-leaf/user-reviews/'
 driver.get(url)
+
+driver.maximize_window()
+
+#The whole page is dynamically loaded, scroll to the bottom to load all the reviews
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+while True:
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+    time.sleep(2)
+
+    new_height = driver.execute_script("return document.body.scrollHeight")
+
+    if(new_height == last_height):
+        break
+
+    else:
+        last_height = new_height
 
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
