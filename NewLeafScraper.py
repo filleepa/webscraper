1db3c7968c6b54da9fd31426c8d2e4c1f15c90b6
filleepa <78672802+filleepa@users.noreply.github.com>
2025-01-22 16:30:20 +0000

    Initial test code successfully scraping the data I need (name, date, score, review) from the html

---
+++ NewLeafScraper.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager  # Automatically manages ChromeDriver
+
+# ChromeDriver set up with Service
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+
+url = 'https://www.metacritic.com/game/animal-crossing-new-leaf/user-reviews/'
+driver.get(url)
+
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+review_dict = {'name':[], 'date':[], 'rating':[], 'review':[]}
+
+for review in soup.find_all('div', class_='c-siteReview_main'):
+    try:
+        review_dict['name'].append(review.find('a', class_='c-siteReviewHeader_username').text.strip())
+    except AttributeError:
+        review_dict['name'].append(None)
+    try:
+        review_dict['date'].append(review.find('div', class_='c-siteReviewHeader_reviewDate').text.strip())
+    except AttributeError:
+        review_dict['date'].append(None)
+    try:
+        review_dict['rating'].append(review.find('div', class_='c-siteReviewHeader_reviewScore').find_all('div')[0].text)
+    except AttributeError:
+        review_dict['rating'].append(None)
+    try:
+        review_dict['review'].append(review.find('div', class_='c-siteReview_quote').find('span').text)
+    except AttributeError:
+        review_dict['review'].append(None)
+
+df = pd.DataFrame(review_dict)
+
+df.to_csv('test.csv')
